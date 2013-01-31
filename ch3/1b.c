7f3fdb5423056d00de977c7516b5e2b81e0cd480
47dev47null <0x2fdev0x2fnull@gmail.com>
2013-01-31 20:04:58 +0800

    add Andrew Tesker's solution to Exercise 3-1

---
+++ ch3/1b.c
@@ -0,0 +1,53 @@
+/* Solution by Andrew Tesker
+ * [src](http://users.powernet.co.uk/eton/kandr2/krx301.html)
+ */
+
+/* Exercise 3-1
+ * Our binary search makes two tests inside the loop,
+ * when one would suffice (at the price of more tests outside).
+ * Write a version with only one test inside the loop
+ * and measure the difference in run-time.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+int binsearch(int x, int v[], int n);
+
+int main(void)
+{
+	int array[] = {1, 4, 6, 8, 9, 11, 15};
+	int x1 = 1;
+	int x2 = 6;
+	int x3 = 15;
+	int x4 = 0;
+	int x5 = 16;
+
+	printf("%d\n", binsearch(x1, array, 7));	/* expect: 0 */
+	printf("%d\n", binsearch(x2, array, 7));	/* expect: 2 */
+	printf("%d\n", binsearch(x3, array, 7));	/* expect: 6 */
+	printf("%d\n", binsearch(x4, array, 7));	/* expect: -1 */
+	printf("%d\n", binsearch(x5, array, 7)); 	/* expect: -1 */
+
+	return 0;
+}
+
+int binsearch(int x, int v[], int n)
+{
+	int low, high, mid;
+
+	low = 0;
+	high = n-1;
+
+	while(low < high)
+	{
+    	mid = (low + high) / 2;
+    	if(x <= v[mid]) 
+    		high = mid;
+    	else 
+      		low = mid + 1;
+  	}
+
+  	return x == v[low] ? low : -1;
+}
